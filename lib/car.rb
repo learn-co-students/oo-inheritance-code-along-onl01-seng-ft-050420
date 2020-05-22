51354365682e6abf8ea467b54fda1c77cdfbfe9c
Irene Scott <irenejinscott@gmail.com>
2020-05-22 14:11:50 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,7 @@
 require_relative "./vehicle.rb"
+class Car < Vehicle #we use the < to inherit the car class from vehicle
+#we didn't write anything in our car class but instances of car all inherit the vehicle methods
+ def go 
+   "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+ end
+end
